fdd880d2bb77aef2ec0ea2247ae83da22ed93f46
YoussefSefiani <youssef.sefiani@student.ehb.be>
2022-06-20 19:48:25 +0200

    host azure

--- src/main/java/fw/ratingservice/RatingServiceApplication.java
+++ src/main/java/fw/ratingservice/RatingServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class RatingServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RatingServiceApplication.class, args);
 	}
